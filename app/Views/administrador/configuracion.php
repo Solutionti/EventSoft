44bfc91017cf63245f91e18ec4d687c88a305c22
Cristian Vargas <ccvargas95@gmail.com>
2026-01-15 22:25:28 -0500

    subiendo cambios con errores

--- app/Views/administrador/configuracion.php
+++ app/Views/administrador/configuracion.php
@@ -16,98 +16,197 @@ precio -->
     <?php require_once 'componentes/head.php'; ?>
   </head>
   <body>
+    <div class="container-scroller">
       <?php require_once 'componentes/navbar.php'; ?>
-      <!--  -->
-      <div class="body flex-grow-1">
-        <div class="row">
-            <div class="col-md-7">
-                <div class="container-lg px-4">
-                  <div class="card mb-4">
-                    <div class="card-header">Lista de Usuarios</div>
-                      <div class="card-body">
-                        <div class="table-responsive">
-                          <table class="table table-striped table-hover">
-                            <thead>
-                              <tr> 
-                                <th class="text-uppercase bg-primary text-white">Documento</th>
-                                <th class="text-uppercase bg-primary text-white">Nombre completo</th>
-                                <th class="text-uppercase bg-primary text-white">telefono</th>
-                                <th class="text-uppercase bg-primary text-white">correo</th>
-                                <th class="text-uppercase bg-primary text-white">rol</th>
-                                <th class="text-uppercase bg-primary text-white">estado</th>
-                                <th class="text-uppercase bg-primary text-white"></th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>MRB001</td>
-                                <td>RUTINA PRINCIPAL</td>
-                                <td>30</td>
-                                <td>85.000</td>
-                                <td>85.000</td>
-                                <td>85.000</td>
-                                <td>
-                                  <div class="dropdown">
-                                    <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v"></i>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                      <a class="dropdown-item" href="#">Ver</a>
-                                      <a class="dropdown-item" href="#">Editar</a>
-                                      <a class="dropdown-item text-danger" href="#">Eliminar</a>
-                                    </div>
-                                  </div>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
+      <div class="container-fluid page-body-wrapper">
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header flex-wrap">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="card-header text-uppercase">configuracion del evento</div>
+                </div>
+                <div class="col-md-6">
+                  <div class="header-left">
+                    <button class="btn btn-primary mb-2 mb-md-0 me-2 btn-rounded" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Crear Usuarios </button>
+                    <button class="btn btn-outline-danger mb-2 mb-md-0 me-2 btn-rounded" data-bs-toggle="modal" data-bs-target="#modalProceso">Crear Regalo</button>
+                    <button
+                      class="btn btn-outline-primary bg-white mb-2 mb-md-0 btn-rounded"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exportarexcelmodal"
+                    >
+                      Crear Servicio
+                    </button>
+                    <button class="btn btn-primary mb-2 mb-md-0 me-2 btn-rounded" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Crear Patrocinio</button>
                   </div>
                 </div>
+              </div>
+              <div class="row mt-5">
+                <div class="col-md-7">
+                  <div class="card-header text-uppercase">Usuarios</div>
+                  <table class="table table-striped table-borderless mt-3">
+                    <thead>
+                      <tr>
+                        <th class="color-morado text-white text-uppercase"></th> 
+                        <th class="color-morado text-white text-uppercase">Documento</th>
+                        <th class="color-morado text-white text-uppercase">Nombre completo</th>
+                        <th class="color-morado text-white text-uppercase">telefono</th>
+                        <th class="color-morado text-white text-uppercase">correo</th>
+                        <th class="color-morado text-white text-uppercase">rol</th>
+                        <th class="color-morado text-white text-uppercase">estado</th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                        <div class="dropdown">
+                            <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="fas fa-ellipsis-v"></i>
+                            </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="#">Ver</a>
+                            <a class="dropdown-item" href="#">Editar</a>
+                            <a class="dropdown-item text-danger" href="#">Eliminar</a>
+                          </div>
+                        </div>
+                        </td>
+                        <td>1110542802</td>
+                        <td>JERSON GALVES SUNCHO</td>
+                        <td>3155639791</td>
+                        <td>jerson_galvez@hotmail.com</td>
+                        <td>85.000</td>
+                        <td>85.000</td>
+                      </tr>
+                    </tbody>
+                    </table>
+                </div>
                 <div class="col-md-5">
-                  <div class="container-lg px-4">
-                    <div class="card mb-4">
-                      <div class="card-header">Lista de Codigos Promocionales</div>
-                        <div class="card-body">
-                          <div class="table-responsive">
-                          <table class="table table-striped table-hover">
-                            <thead>
-                              <tr> 
-                                <th class="text-uppercase bg-primary text-white">Deportista</th>
-                                <th class="text-uppercase bg-primary text-white">Codigo</th>
-                                <th class="text-uppercase bg-primary text-white">precio</th>
-                                <th class="text-uppercase bg-primary text-white">estado</th>
-                                <th class="text-uppercase bg-primary text-white"></th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>MRB001</td>
-                                <td>RUTINA PRINCIPAL</td>
-                                <td>30</td>
-                                <td>85.000</td>
-                                <td>
-                                  <div class="dropdown">
-                                    <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v"></i>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-end">
-                                      <a class="dropdown-item" href="#">Ver</a>
-                                      <a class="dropdown-item" href="#">Editar</a>
-                                      <a class="dropdown-item text-danger" href="#">Eliminar</a>
-                                    </div>
-                                  </div>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
+                  <div class="card-header text-uppercase">codigos promocionales</div>
+                  <table class="table table-striped table-borderless mt-3">
+                    <thead>
+                      <tr>
+                        <th class="color-morado text-white text-uppercase"></th> 
+                        <th class="color-morado text-white text-uppercase">Deportista</th>
+                        <th class="color-morado text-white text-uppercase">Codigo</th>
+                        <th class="color-morado text-white text-uppercase">precio</th>
+                        <th class="color-morado text-white text-uppercase">estado</th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                        <div class="dropdown">
+                            <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="fas fa-ellipsis-v"></i>
+                            </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="#">Ver</a>
+                            <a class="dropdown-item" href="#">Editar</a>
+                            <a class="dropdown-item text-danger" href="#">Eliminar</a>
+                          </div>
                         </div>
+                        </td>
+                        <td>1110542802</td>
+                        <td>NTU1Q2C</td>
+                        <td>300000</td>
+                        <td>ACTIVO</td>
+                      </tr>
+                    </tbody>
+                    </table>
+              </div>
+              <div class="row mt-5">
+                <div class="col-md-5">
+                  <div class="card-header text-uppercase">patrocinios</div>
+                  <table class="table table-striped table-borderless mt-3">
+                    <thead>
+                      <tr>
+                        <th class="color-morado text-white text-uppercase"></th> 
+                        <th class="color-morado text-white text-uppercase">logo</th>
+                        <th class="color-morado text-white text-uppercase">nombre</th>
+                        <th class="color-morado text-white text-uppercase">descripcion</th>
+                        <th class="color-morado text-white text-uppercase">valor aporte</th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                        <div class="dropdown">
+                            <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="fas fa-ellipsis-v"></i>
+                            </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="#">Ver</a>
+                            <a class="dropdown-item" href="#">Editar</a>
+                            <a class="dropdown-item text-danger" href="#">Eliminar</a>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
+                        </td>
+                        <td>MRB001</td>
+                        <td>RUTINA PRINCIPAL</td>
+                        <td>30</td>
+                        <td>85.000</td>
+                      </tr>
+                    </tbody>
+                    </table>
+                </div>
+                <div class="col-md-7">
+                  <div class="card-header text-uppercase">productos o servicios</div>
+                  <table class="table table-striped table-borderless mt-3">
+                    <thead>
+                      <tr>
+                        <th class="color-morado text-white text-uppercase"></th> 
+                        <th class="color-morado text-white text-uppercase">imagen</th>
+                        <th class="color-morado text-white text-uppercase">Codigo</th>
+                        <th class="color-morado text-white text-uppercase">nombre producto</th>
+                        <th class="color-morado text-white text-uppercase">precio</th>
+                        <th class="color-morado text-white text-uppercase">vistas</th>
+                        <th class="color-morado text-white text-uppercase">estado</th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                        <div class="dropdown">
+                            <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="fas fa-ellipsis-v"></i>
+                            </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="#">Ver</a>
+                            <a class="dropdown-item" href="#">Editar</a>
+                            <a class="dropdown-item text-danger" href="#">Eliminar</a>
+                          </div>
+                        </div>
+                        </td>
+                        <td>IMAGEN</td>
+                        <td>1</td>
+                        <td>CORRER 1</td>
+                        <td>300.000</td>
+                        <td>5</td>
+                        <td>ACTIVO</td>
+                      </tr>
+                    </tbody>
+                    </table>
+                </div>
+              </div>
+
+
+              
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      <!--  -->
+
+      <div class="body flex-grow-1">
+        <div class="row">
+                          
+                        
                 </div>
               </div>
             <?php require_once 'componentes/footer.php'; ?>
